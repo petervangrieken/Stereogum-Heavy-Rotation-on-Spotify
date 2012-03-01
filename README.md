6ebcc5603b297706db14316c9b00cd0f2673ef51
Peter van Grieken <peter@usevine.nl>
2012-03-01 14:20:29 +0100

    Initial commit
    Signed-off-by: Peter van Grieken <peter@usevine.nl>

---
+++ README.md
@@ -0,0 +1,26 @@
+# Stereogum Heavy Rotation Spotify App
+## Background
+This unofficial(!) App is made to facilitate music lovers need for fresh music. One of my personal sources of new material is the Heavy Rotation section of Strereogum. Because Stereogum doesn't provide any easy way to listen to these releases, I've decided to create this little App. The App looks up all the mentioned releases of Heavy Rotation and searches for these releases on Spotify. Because not all releases are available on Spotify (or only after a certain time) it's quite probable the application doesn't show all new releases.
+
+
+## Thanks
+Thank you Peter Watts, for creating the Spotify KitchenSink. This skeleton project made it so much easier for me to create this application. If it wasn't for your efforts I'd probably given up sooner or later.
+
+## Disclaimers
+
+This is an unofficial application, not sponsored or endorsed by Spotify or any of the mentioned music blogs. If you wish to develop for Spotify Apps, you must sign up for their Developer Program, and abide by their rules.
+
+## Contact
+
+### Stereogum Heavy Rotation App
+jan at janhoogeveen dot eu // [@janhoogeveen](http://twitter.com/janhoogeveen)
+
+
+### KitchenSink
+pwattsmail at gmail dot com // [@ptrwtts](http://twitter.com/ptrwtts)
+andrew at deceptacle dot com // [@andrewburgess](http://twitter.com/andrewburgess)
+
+
+## License
+Distributed under the MIT license. Copyright (c) 2011 Peter Watts
+Additional development by Jan Hoogeveen in 2012 for Usevine.
---
+++ js/search.js
@@ -0,0 +1,96 @@
+var asyncCalls = [],  // Initiate for later
+	tempPlaylist = new models.Playlist();
+
+var $artist = '';
+var $album = '';
+var $spotifyuri = '';
+
+//DOM READY
+$(function(){
+
+	//Laad de JSON op met artiesten en albums
+	$.getJSON('http://janhoogeveen.eu/heavyrotation/heavyrotation.json', function(data) {
+
+		$.each(data, function(key, val) {
+			//Lege velden slaan we over
+			if (key == 'undefined' || key == '') {
+			//Dit zijn de albums
+			} else {
+				var $artist = val[0];
+				var $album = val[1];
+				//Als de albumnaam niet is ingevuld proberen we een self-titled album te vinden
+				if ($album == '' || $album == 'undefined' || $album == null || $album == 0) {
+				 $album = $artist;
+				}
+				
+				//Nu gaan we zoeken
+				var search = new models.Search('artist:'+$artist+' '+'album:'+$album+' '+'year:2011-2012');
+				
+				search.localResults = models.LOCALSEARCHRESULTS.APPEND; // Local files last
+			
+				//Zoekresultaten komen binnen
+				search.observe(models.EVENT.CHANGE, function() {
+						
+					//Maak een lijst met tracks aan
+					if(search.tracks.length) {
+						tempPlaylist = new models.Playlist();
+						
+						var $artisturi = '';
+						var $albumuri = '';
+						
+						$.each(search.tracks,function(index,track){
+						
+							if (track.album.artist.name.toLowerCase() == $artist.toLowerCase()) {
+							
+								$artisturi = track.album.artist.uri;
+								$albumuri = track.album.uri;
+							
+								tempPlaylist.add(models.Track.fromURI(track.uri)); // Note: artwork is compiled from first few tracks. if any are local it will fail to generate....
+							}
+
+						});
+
+						
+						var playlistArt = new views.Player();
+
+						playlistArt.track = tempPlaylist.get(0);
+						playlistArt.context = tempPlaylist;
+						
+						console.log($artisturi);
+						console.log($albumuri);
+						
+						$('#track-results').append('<h2><strong><a href="'+$artisturi+'">'+$artist+'</a></strong> - <a href="'+$albumuri+'">'+$album+'</a></h2>');
+						$('#track-results').append('<div class="tracklistcontainer"></div>');
+						$("#track-results .tracklistcontainer:last-child").append(playlistArt.node);
+	
+						var playlistList = new views.List(tempPlaylist);
+						playlistList.node.classList.add("temporary");
+						playlistList.node.classList.add("sp-light");
+						$("#track-results .tracklistcontainer:last-child").append(playlistList.node);
+
+					}
+				});
+				search.appendNext();
+
+			} //Data was niet leeg
+		}); //each data item
+	}); //GetJSON
+
+
+
+
+
+}); //DOM
+
+
+
+//Iets is klaar
+function asyncComplete(key) {
+	asyncCalls.splice(asyncCalls.indexOf(key), 1);
+	if(asyncCalls.length==0) {
+		console.log('All async calls home safely'); // <insert action that requires all async calls>
+	} else {
+		console.log(asyncCalls.length+" aysnc calls remaining");
+	}
+	// Obviously in production you would want a more robust solution that can handle calls that fail!
+}
